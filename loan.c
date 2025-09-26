f2d503fc582a7821d433ce378f36bb5105e84f28
lupez-jymo <jameswanjiru0704@gmail.com>
2025-09-26 11:58:37 +0300

    Add files via upload

---
+++ loan.c
@@ -0,0 +1,30 @@
+/*
+Name:james ngugi
+reg no:CT101/G/26469/25.
+Description:loan application qualification
+*/
+#include <stdio.h>
+
+int main() {
+    int age;
+    float income;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+   
+    printf("Enter your annual income (in Sh): ");
+    scanf("%f", &income);
+
+    
+    if (age >= 21 && income >= 21000) {
+        printf("Congratulations, you qualify for a loan.\n");
+    } else {
+        printf("Unfortunately, we are unable to offer you a loan at this time.\n");
+    }
+
+return 0;
+}
+
+
+
